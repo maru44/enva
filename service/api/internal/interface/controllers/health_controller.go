6c208d38f2b639fc87b06b9b93950b3077a7c4d3
maru44 <takumaru22244@gmail.com>
2022-01-31 08:19:38 +0900

    safe

--- service/api/internal/interface/controllers/health_controller.go
+++ service/api/internal/interface/controllers/health_controller.go
@@ -1,11 +1,9 @@
 package controllers
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/maru44/enva/service/api/internal/interface/database"
-	"github.com/maru44/enva/service/api/pkg/config"
 	"github.com/maru44/perr"
 )
 
@@ -22,12 +20,12 @@ func NewHealthController(sql database.ISqlHandler) *HealthController {
 func (con *HealthController) PostgresCheckView(w http.ResponseWriter, r *http.Request) {
 	row := con.sql.QueryRowContext(r.Context(), "SELECT '1'")
 	if err := row.Err(); err != nil {
-		response(w, r, perr.Wrap(err, perr.InternalServerErrorWithUrgency, fmt.Sprintf("connection refused to postgresql: %s", config.POSTGRES_URL)), nil)
+		response(w, r, perr.Wrap(err, perr.InternalServerErrorWithUrgency), nil)
 		return
 	}
 	var one string
 	if err := row.Scan(&one); err != nil {
-		response(w, r, perr.Wrap(err, perr.InternalServerErrorWithUrgency, fmt.Sprintf("connection refused to postgresql: %s", config.POSTGRES_URL)), nil)
+		response(w, r, perr.Wrap(err, perr.InternalServerErrorWithUrgency), nil)
 		return
 	}
 	response(w, r, nil, map[string]interface{}{"data": one})
