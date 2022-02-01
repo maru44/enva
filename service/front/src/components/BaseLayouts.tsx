ec879e38eb54efba39440e13a6a752034d560c05
maru44 <takumaru22244@gmail.com>
2022-02-02 05:51:54 +0900

    modify front

--- service/front/src/components/BaseLayouts.tsx
+++ service/front/src/components/BaseLayouts.tsx
@@ -12,27 +12,29 @@ export const BaseLayout: React.FC<props> = ({ main }) => {
   return (
     <Box>
       <Head>
-        <title>Envassador</title>
+        <title>Envassador | Acceralate Development!</title>
         <meta
           property="og:title"
-          content="Envassador | Acceralate Local Development!"
+          content="Envassador | Acceralate Development!"
         />
         <meta property="og:type" content="website" />
-        {/* @TODO fix description */}
         <meta
           property="og:description"
-          content="Envassador serves you an efficient system to share Environment variable. Envassador helps your local development."
+          content="Envassador serves you an efficient system to share environment variable. Envassador makes your development more comfortable."
         />
         <meta
           property="description"
-          content="Envassador serves you an efficient system to share Environment variable. Envassador helps your local development."
+          content="Envassador serves you an efficient system to share environment variable. Envassador makes your development more comfortable."
         />
         <meta property="og:url" content={process.env.NEXT_PUBLIC_FRONT_URL} />
-        <meta property="og:image" />
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          property="og:image"
+          content={`${process.env.NEXT_PUBLIC_FRONT_URL}/enva.png`}
+        />
+        <meta name="twitter:card" content="summary" />
         <meta
           name="twitter:title"
-          content="Envassador | Acceralate Local Development!"
+          content="Envassador | Acceralate Development!"
         />
         <link rel="icon" href="/enva.png" />
       </Head>
