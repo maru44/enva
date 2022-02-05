6221faae40622a43eca1e367e1f3a6b38079613a
maru44 <takumaru22244@gmail.com>
2022-02-05 11:42:56 +0900

    ok maybe

--- service/front/src/pages/api/auth/refresh.ts
+++ service/front/src/pages/api/auth/refresh.ts
@@ -43,7 +43,7 @@ export default async function handler(
             getCookieOption(3600 * 24 * 7 * 3)
           ),
         ])
-        res.status(200).json({ message: ret.id_token })
+        res.status(200).json({ message: 'succeeded to refresh token' })
         return
       default:
         res.status(400).json(ret)
